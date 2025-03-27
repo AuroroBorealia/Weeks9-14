9af816210c1ece869c665712baaf3d95adddf8f5
Kira Stevens <stevekir@shernet.sheridancollege.ca>
2025-03-27 14:02:35 -0400

    Did animaiton event

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    SpriteRenderer sr;
+    Animator animator;
+    public float speed = 2;
+    public bool canRUn = true;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+
+        sr.flipX = (direction < 0);
+        animator.SetFloat("movement", Mathf.Abs(direction));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Attack");
+            canRUn = false;
+        }
+
+        if(canRUn == true)
+        {
+            transform.position += transform.right * direction * speed * Time.deltaTime;
+
+        }
+
+    }
+
+    public void AttackHasFinished()
+    {
+        Debug.Log("The attack animation has just finished!");
+        canRUn = true;
+    }
+}
