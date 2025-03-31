8b5e8bc6516291827c3623b0295143d9f19e86c6
Kira Stevens <stevekir@shernet.sheridancollege.ca>
2025-03-31 13:32:50 -0400

    Commented my code and added collider circles to the sprites

--- Assets/SystemProject/Scripts/Player.cs
+++ Assets/SystemProject/Scripts/Player.cs
@@ -16,24 +16,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //directions for making the player move horizontally and vertically
         float directionX = Input.GetAxis("Horizontal");
         float directionY = Input.GetAxis("Vertical");
 
+        //Make the player move right with the arrow key
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += transform.right * directionX * speed * Time.deltaTime;
         }
 
+        //Make the player move left with the arrow key
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += transform.right * directionX * speed * Time.deltaTime;
         }
 
+        //Make the player move up with the arrow key
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += transform.up * directionY * speed * Time.deltaTime;
         }
 
+        //make the player move down with the arrow key
         if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.position += transform.up * directionY * speed * Time.deltaTime;
