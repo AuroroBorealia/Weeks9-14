882e1ffb761635cba718f2b521b0219754c3fb56
Kira Stevens <stevekir@shernet.sheridancollege.ca>
2025-03-31 12:24:53 -0400

    Made Player movement

---
+++ Assets/SystemProject/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float directionX = Input.GetAxis("Horizontal");
+        float directionY = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * directionX * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += transform.right * directionX * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.up * directionY * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += transform.up * directionY * speed * Time.deltaTime;
+        }
+    }
+}
